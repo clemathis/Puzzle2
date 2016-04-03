1684e214adae31cdadb04453014b7683a592cbd1
Agnieszka <agnieszka.burek12@gmail.com>
2016-04-03 20:19:17 +0200

    Test changes

---
+++ app/src/main/java/aga/puzzle2/Playground.java
@@ -0,0 +1,14 @@
+package aga.puzzle2;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Playground extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playground);
+    }
+}
