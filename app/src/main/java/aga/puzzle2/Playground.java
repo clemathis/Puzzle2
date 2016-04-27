1f0670fb13174e446635b994ea257d68e0163bdb
Agnieszka <agnieszka.burek12@gmail.com>
2016-04-27 22:01:57 +0200

    Saving the last piece.

--- app/src/main/java/aga/puzzle2/Playground.java
+++ app/src/main/java/aga/puzzle2/Playground.java
@@ -5,10 +5,17 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Playground extends Activity {
@@ -23,6 +30,53 @@ public class Playground extends Activity {
         // Note: This will set the number of splits to 9 by default, if the user does not select the radio button.
         nickname_tip.setText(getIntent().getStringExtra("name")+", welcome!");
         ArrayList<Bitmap> numberOfSplits = splitImage(Integer.parseInt(getIntent().getStringExtra("numberOfSplits")));
+        Bitmap lastImagePiece = numberOfSplits.get(numberOfSplits.size());
+
+        FileOutputStream fileOutputStream = null;
+        try {
+
+            // create a File object for the parent directory
+            File lastPiece = new File("/sdcard/Capture/");
+            // have the object build the directory structure, if needed.
+            lastPiece.mkdirs();
+
+            //Capture is folder name and file name with date and time
+            fileOutputStream = new FileOutputStream(String.format(
+                    "/sdcard/Capture/%d.jpg",
+                    System.currentTimeMillis()));
+
+            // Here we Resize the Image ...
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            lastImagePiece.compress(Bitmap.CompressFormat.JPEG, 100,
+                    byteArrayOutputStream); // bm is the bitmap object
+            byte[] bsResized = byteArrayOutputStream.toByteArray();
+
+            fileOutputStream.write(bsResized);
+            fileOutputStream.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+        //Removing the last piece
+        numberOfSplits.remove(numberOfSplits.size());
+
+
+        /*
+	* 1. Bitmap lastImagePiece = numberOfSplits.get(numberOfSplits.size());
+	* 2. Remove the last piece : numberOfSplits.remove(numberOfSplits.seize());
+	* 3. Opening the FileOutputStream fos : file name : lastImage.jpeg
+	* 4. Writing the Bitmap lastImagePiece to fos
+	* 5. fos.flush();
+	* 6. fos.close();
+	* 7. ArrayList<Bitmap> shuffledImages = shuffle(numberOfSplits);
+	*/
+
+
         GridView grid = (GridView) findViewById(R.id.gridview);
         grid.setAdapter(new ImageAdapter(this, numberOfSplits));
         grid.setNumColumns((int) Math.sqrt(numberOfSplits.size()));
