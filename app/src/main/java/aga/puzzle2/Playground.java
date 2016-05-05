240e5bbff6f1c5008b2f566deae3fc8396bc80e1
Agnieszka <agnieszka.burek12@gmail.com>
2016-05-05 22:02:18 +0200

    Trying to move the splits + validation and popup.

--- app/src/main/java/aga/puzzle2/Playground.java
+++ app/src/main/java/aga/puzzle2/Playground.java
@@ -1,7 +1,8 @@
 package aga.puzzle2;
 
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -11,10 +12,15 @@ import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -26,81 +32,95 @@ import java.util.List;
 
 public class Playground extends Activity {
 
-    int score = 500;
-    int moves = 0;
-
+    int SCORE = 500;
+    int MOVES = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playground);
 
-        String number_of_score = Integer.toString(score);
-        TextView nr_of_score = (TextView) findViewById(R.id.nr_of_scores);
-        nr_of_score.setText(number_of_score);
+        // private String selectedImagePath;
+        TextView nickname_tip = (TextView) findViewById(R.id.name);
+        //This gets the elements from the New game page and sets it in the Playground page.
+        // Note: This will set the number of splits to 9 by default, if the user does not select the radio button.
+        final int splits = Integer.parseInt(getIntent().getStringExtra("numberOfSplits"));
+        nickname_tip.setText(getIntent().getStringExtra("name")+", welcome!");
+        final ArrayList<Bitmap> numberOfSplits = splitImage(splits);
+        Bitmap lastImagePiece = numberOfSplits.get(numberOfSplits.size() - 1);
+        //numberOfSplits.remove(numberOfSplits.size()-1);
+
+        //Shuffeling
+        lastImagePiece.eraseColor(android.graphics.Color.BLACK);
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        final Bitmap bmp = Bitmap.createBitmap(lastImagePiece.getWidth(), lastImagePiece.getHeight(), conf);
+        numberOfSplits.set(numberOfSplits.size() - 1, bmp);
+        Collections.shuffle(numberOfSplits);
+
+        final GridView grid = (GridView) findViewById(R.id.gridview);
+        final ImageAdapter SIA = new ImageAdapter(this, numberOfSplits);
+        int changedPosition = 0;
+        grid.setAdapter(SIA);
+        grid.setNumColumns((int) Math.sqrt(numberOfSplits.size()));
+        grid.setHorizontalSpacing(0);
+        grid.setVerticalSpacing(0);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                int up = position - 3;
+                int down = position + 3;
+                int left = position - 1;
+                int right = position + 1;
+                ImageAdapter ia = (ImageAdapter) parent.getAdapter();
+                ImageView imageView = (ImageView) v;
+
+                if (up >= 0 && ia.getItem(up).equals(bmp)) {
+
+                    //       ((ImageView) parent.getItemAtPosition(up)).setImageResource(SIA.getItemViewType(position));
+                    imageView.setImageResource(SIA.getItemViewType(up));
+
+                } else if (down <= (splits - 1) && ia.getItem(down).equals(bmp)) {
+
+//                    ((ImageView) parent.getItemAtPosition(down)).setImageResource(SIA.getItemViewType(position));
+                    imageView.setImageResource(SIA.getItemViewType(down));
+
+                } else if (left >= 0 && ia.getItem(left).equals(bmp)) {
+
+//                    ((ImageView) parent.getItemAtPosition(left)).setImageResource(SIA.getItemViewType(position));
+                    imageView.setImageResource(SIA.getItemViewType(left));
+
+                } else if (right <= (splits - 1) && ia.getItem(right).equals(bmp)) {
+
+//                    ((ImageView) parent.getItemAtPosition(right)).setImageResource(SIA.getItemViewType(position));
+                    imageView.setImageResource(SIA.getItemViewType(right));
+
+                }
+                Toast.makeText(getApplicationContext(),
+                        ((position + 1) + " " + (position - 1) + " " + position),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
         Button submit = (Button) findViewById(R.id.finish_button);
         submit.setOnClickListener(new View.OnClickListener() {
                                       public void onClick(View v) {
                                           Intent intent = new Intent(v.getContext(), FinishGame.class);
+                                          ArrayList validationList = new ArrayList();
+                                          Collections.copy(numberOfSplits, validationList);
+                                          Collections.sort(validationList);
+                                          if (numberOfSplits.equals(validationList))
                                           startActivityForResult(intent, 0);
+                                          else {
+                                              AlertDialog.Builder alert = new AlertDialog.Builder(Playground.this);
+                                              alert.setTitle("Unfortunately!");
+                                              alert.setMessage("Splits are not in the correct order.");
+                                              alert.setPositiveButton("OK", null);
+                                              alert.show();
+                                          }
                                       }
                                   }
         );
-        // private String selectedImagePath;
-        TextView nickname_tip = (TextView) findViewById(R.id.name);
-        //This gets the elements from the New game page and sets it in the Playground page.
-        // Note: This will set the number of splits to 9 by default, if the user does not select the radio button.
-        nickname_tip.setText(getIntent().getStringExtra("name") + ", welcome!");
-
-        ArrayList<Bitmap> numberOfSplits = splitImage(Integer.parseInt(getIntent().getStringExtra("numberOfSplits")));
-        Bitmap lastImagePiece = numberOfSplits.get(numberOfSplits.size() - 1);
-
-        FileOutputStream fileOutputStream = null;
-        try {
-
-            // create a File object for the parent directory
-            File path = Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS);
-            File lastPiece = new File(path, "lastPiece.jpg");
-            try {
-                // Make sure the Pictures directory exists.
-                path.mkdirs();
-                lastPiece.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // have the object build the directory structure, if needed.
-
-            //Capture is folder name and file name with date and time
-            fileOutputStream = new FileOutputStream(lastPiece);
-
-            // Here we Resize the Image ...
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            lastImagePiece.compress(Bitmap.CompressFormat.JPEG, 100,
-                    byteArrayOutputStream); // bm is the bitmap object
-            byte[] bsResized = byteArrayOutputStream.toByteArray();
-
-            fileOutputStream.write(bsResized);
-            fileOutputStream.close();
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-        }
-
-        //Removing the last piece
-        //numberOfSplits.remove(numberOfSplits.size()-1);
-        lastImagePiece.eraseColor(android.graphics.Color.BLACK);
-        //Shuffeling
-        Collections.shuffle(numberOfSplits);
-
-        GridView grid = (GridView) findViewById(R.id.gridview);
-        grid.setAdapter(new ImageAdapter(this, numberOfSplits));
-        grid.setNumColumns((int) Math.sqrt(numberOfSplits.size()));
-
 
     }
     private ArrayList<Bitmap> splitImage(int NumberOfSplits) {
@@ -132,15 +152,16 @@ public class Playground extends Activity {
             yCoord += chunkHeight;
         }
         return chunkedImages;
+
+
     }
 
-    //public moves OnClick(grid){
-        //moves ++;
-        //return moves;
-    //}
+    //    public moves OnClinck(grid){
+//        MOVES ++;
+//    }
+    public void calculateScore(int MOVES){
 
-    public void calculateScore(int moves){
-        score = score -(moves *10);
+        SCORE = SCORE -(MOVES *10);
     }
 
 
